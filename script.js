61a28ceb5ea642ad3c7e853b99f19625d488246f
ehhhahah <103206211+ehhhahah@users.noreply.github.com>
2022-04-24 17:46:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const CATEGORIES = ['Physic', 'bigTech', 'classic', 'openSource', 'descriptive', 'AI', 'commercial', 'FreeSound', 'DIY', 'math', 'mustCheck']
+const CATEGORIES = ["classic", "physic", "openSource", "commercial", "FreeSound", "tonejs", "DIY", "AI", "ambient", "TeroParviainen", "descriptive", "math", "DAW", "bigTech", "classical", "mustCheck"]
 
 function hideShowClassElement(className) {
     let currState = document.getElementsByClassName(`toggle-${className}`)[0].classList.toggle('toggle-on');
@@ -8,3 +8,30 @@ function hideShowClassElement(className) {
         elements[i].style.display = currState ? "block" : "none"
     }
 }
+
+function renderFilteringButtons() {
+    CATEGORIES.forEach((category) => {
+        const btn = document.createElement("button");
+        btn.innerHTML = "#" + category;
+        btn.type = "button"
+        btn.classList = "button is-rounded  toggle toggle-on toggle-" + category
+        btn.onclick = function() { hideShowClassElement(category); };
+        const filteringSpan = document.getElementsByClassName("filtering")[0]
+        filteringSpan.appendChild(btn);
+    })
+}
+
+function toggleAll() {
+    let currState = document.getElementsByClassName("toggle-all")[0].classList.toggle('toggle-all-off');
+    CATEGORIES.forEach((className) => {
+        if (currState) document.getElementsByClassName(`toggle-${className}`)[0].classList.add('toggle-on');
+        else document.getElementsByClassName(`toggle-${className}`)[0].classList.remove('toggle-on');
+        const elements = document.getElementsByClassName(className);
+
+        for (let i=0; i<elements.length; i++) {
+            elements[i].style.display = currState ? "block" : "none"
+        }
+    })
+}
+
+window.onload = renderFilteringButtons
